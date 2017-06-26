6d567cf374ff83095d7973dacac99d08eef4497a
Jan Ertl <ertl@uchicago.edu>
2017-06-26 06:41:23 -0500

    Uploading Homework

---
+++ ProbSets/Comp/calculator.py
@@ -0,0 +1,12 @@
+# calculator.py
+"""Sum, Product, and square root functions"""
+from math import sqrt
+
+def root(a):
+    return sqrt(a)
+
+def sum(a, b):
+    return a + b
+
+def prod(a, b):
+    return a * b
